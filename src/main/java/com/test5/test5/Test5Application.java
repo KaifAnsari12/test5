2aa7d042003542265d23986dde5ba777d9029673
KaifAnsari12 <kaif47780@gmail.com>
2025-03-29 14:28:13 +0530

    first commit

--- src/main/java/com/test5/test5/Test5Application.java
+++ src/main/java/com/test5/test5/Test5Application.java
@@ -8,7 +8,9 @@ public class Test5Application {
 
 	public static void main(String[] args) {
               String name="mike";
-              int x=100;
+              int x=100; 
+               int y=200;
+              double z=10.3;
                boolean isPresent=false;
 		SpringApplication.run(Test5Application.class, args);
 	}
